20bf451e3f0d14bacf87d4e78ee4176a63786bb2
amhchen <abelchen923@gmail.com>
2016-07-20 14:22:38 -0700

    Added dividers (<hr> html tag) between teams

--- templates/my-frontend-form.php
+++ templates/my-frontend-form.php
@@ -12,7 +12,7 @@ if (is_user_logged_in()) { // can only edit a campaign if logged in
             $hasError = true;
         }
 */
-        $c_args = array(
+        $c_args = array( // arguments to query for the most recent active cause
             'post_type'     => 'give_forms',
             'tax_query'     => array(
                 'relation'  => 'AND',
@@ -30,14 +30,17 @@ if (is_user_logged_in()) { // can only edit a campaign if logged in
             'orderby'       => 'post_date',
             'order'         => 'DESC'
         );
+
         $cause = get_posts( $c_args);
+
         if (count($cause) > 0) {
             $cause_target_date = get_post_meta($cause[0]->ID, '_give_target_date', true);
         }
         else {
             wp_die("There aren't any active causes right now!");
         }
-        $args = array(
+
+        $args = array( // arguments to query for most recent active campaign created by current user
             'post_type'		=>	'give_forms',
             'give_forms_category' => 'active',
             'author'        =>  get_current_user_ID(),
@@ -45,6 +48,7 @@ if (is_user_logged_in()) { // can only edit a campaign if logged in
             'order'			=> 'DESC',
             'numberposts'      => 1
         );
+
         $u_posts = get_posts( $args );
 
         $post_info = array(
@@ -53,11 +57,12 @@ if (is_user_logged_in()) { // can only edit a campaign if logged in
             'post_status'=>'publish',
             'post_excerpt' => $_POST['postcontent']
         );
-        $new;
-        $pid;
+
+        $pid = 0;
+        $new = false;
         if (count($u_posts) == 0)  {
             $pid = wp_insert_post($post_info);
-            $new = true;
+            $new = true; // no active campaigns found, it's a new campaign
         }
         else {
             $pid = $u_posts[0]->ID;
@@ -67,12 +72,16 @@ if (is_user_logged_in()) { // can only edit a campaign if logged in
         }
 
         $year = date('Y');
-        $terms = array('active',$year);
-        if (has_term('cause','give_forms_category',$pid)) {
-            $terms[] = 'cause';
+        $terms = array('active',$year); // categories to add to form
+        if (has_term('cause','give_forms_category',$pid)) { //checks to see if form already possesses the 'cause' category
+            $terms[] = 'cause'; //adds the 'cause' category to terms to add
         }
 
-        $prices = array();
+        $prices = array(); // multi-level donations metadata to add
+
+        /* Hardcoded levels, as requested by CCM*/
+        /* Structure is hardcoded to match existing Give infrastructure*/
+
         //$30.00 level
         $prices[] = array('_give_id' => array( 'level_id' => 1),
             '_give_amount' => '30.00',
@@ -89,6 +98,7 @@ if (is_user_logged_in()) { // can only edit a campaign if logged in
             '_give_text' => '',
             '_give_default' => '',);
 
+        // initialize or update metadata to match specs
         wp_set_object_terms($pid,$terms,'give_forms_category');
         update_post_meta($pid,'_give_default_gateway',"global");
         update_post_meta($pid,'_give_price_option',"multi");
@@ -108,52 +118,42 @@ if (is_user_logged_in()) { // can only edit a campaign if logged in
         update_post_meta($pid,'_give_goal_color','#2bc253');
         update_post_meta($pid,'_give_show_register_form','none');
         update_post_meta($pid,'_give_terms_option','none');
-        
+
+        //handles feature image upload
         if($_FILES['feat-image']['size'] > 0) {
             require_once(ABSPATH . 'wp-admin/includes/image.php');
             require_once(ABSPATH . 'wp-admin/includes/file.php');
             require_once(ABSPATH . 'wp-admin/includes/media.php');
 
             $attachment_id = media_handle_upload('feat-image', $pid);
+
             if ($pid && $attachment_id) {
                 $post_success = set_post_thumbnail($pid, $attachment_id);
             }
         }
-        else if (!get_the_post_thumbnail($pid)){
-            $post_success = set_post_thumbnail($pid, 129);
+        else if (!get_the_post_thumbnail($pid)){ // checks to see a feature image is set
+            $post_success = set_post_thumbnail($pid, 129); // hardcoded default image
         }
-        $redirect = get_home_url();
-        if ( !is_wp_error($pid)) {
+
+        $redirect = get_home_url(); // if all else fails, redirect to homepage
+
+        if ( !is_wp_error($pid)) { // check if add or update was successful
             $redirect = get_permalink($pid);
+
             if ($new) {
-                $user = wp_get_current_user();
-                $message  = "Dear " . $user->user_firstname . ",\n";
-                $message .= "Thank you for signing up for " . $cause[0]->post_title . ".";
-                $message .= "You can share your new campaign page using the following link: \n";
-                $message .= $redirect . "\n\n";
-                $message .= "This is your user ID: " . $user->ID . "\n";
-                $message .= "Please write it on the envelope you were given when you signed up.\n\n";
-                $message .= "Happy fundraising, and see you on " . date('F d',$cause_target_date) . "!\n\n";
-                $message .= "Sincerely,\n";
-                $message .= "CCM Canada Walk for Love " . $year . "\n";
-                $message .= "中信中心愛履同行 " . $year . "\n";
-                $from[]   = "From: abelchen@ymail.com";
-                //add_filter('wp_mail_from','custom_wp_mail_from');
-                //add_filter('wp_mail_from_name','custom_wp_mail_from_name',99);
+                // If it was a new campaign, this action fires after all defaults have been set.
                 do_action('new_campaign_created',$redirect);
-                //remove_filter('wp_mail_from','custom_wp_mail_from');
-                //remove_filter('wp_mail_from_name','custom_wp_mail_from_name',99 );
             }
         }
+
         wp_redirect( $redirect );
         exit();
     }
-    function custom_wp_mail_from($email) {
-        return 'abelchen@ymail.com';
-    }
-    function custom_wp_mail_from_name($name) {
-        return 'CCM Canada';
-    }
+
+    /*
+    This function searches for the most recent active campaign created by the current user
+    and echoes the specified parameter. If no such campaign or parameter exists, nothing happens.
+    */
     function campaign_info($param) {
         $args = array(
             'post_type'		=>	'give_forms',
@@ -163,11 +163,9 @@ if (is_user_logged_in()) { // can only edit a campaign if logged in
             'order'			=> 'DESC',
             'numberposts'      => 1
         );
-        //echo get_current_user_ID();
         $u_posts = get_posts( $args );
         if (count($u_posts) > 0) {
             $pid = $u_posts[0]->ID;
-            //echo $pid;
             switch($param) {
                 case "title":
                     echo get_the_title($pid);
@@ -180,10 +178,9 @@ if (is_user_logged_in()) { // can only edit a campaign if logged in
                     $goal = get_post_meta($pid,"_give_set_goal");
                     foreach ($goal as $num) {echo $num;}
                     break;
+                default:
+                    return;
             }
-        }
-        else {
-            // echo $param;
         }
     }
     ?>
