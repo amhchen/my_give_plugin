fbe02d0bfca0191d3fa3f4eaa5200f60a33f64a0
amhchen <abelchen923@gmail.com>
2016-08-03 12:12:16 -0700

    Added extra row to the 'export' tab's table. Option to export basic stats about teams and individuals and their earnings. Added UFT-8 character support (for chinese characters)

--- includes/admin/export-functions.php
+++ includes/admin/export-functions.php
@@ -76,7 +76,7 @@ add_action( 'give_email_export', 'give_export_all_donors' );
 function download_send_headers($filename) {
 	// disable caching
 	$now = gmdate("D, d M Y H:i:s");
-	header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
+	//header("Expires: Tue, 03 Jul 2001 06:00:00 GMT");
 	header("Cache-Control: max-age=0, no-cache, must-revalidate, proxy-revalidate");
 	header("Last-Modified: {$now} GMT");
 
@@ -90,13 +90,14 @@ function download_send_headers($filename) {
 	header("Content-Transfer-Encoding: binary");
 }
 
-function array2csv(array &$array) {
+function array2csv(array &$array,$col_names) {
 	if (count($array) == 0) {
 		return null;
 	}
 	ob_start();
+	echo "\xEF\xBB\xBF"; // UTF-8 BOM
 	$df = fopen("php://output", 'w');
-	fputcsv($df, array('User ID','Campaign Name'));
+	fputcsv($df, $col_names);
 	foreach ($array as $row) {
 		fputcsv($df, $row);
 	}
@@ -124,22 +125,161 @@ function find_post($user_id) {
 		'orderby'       => 'post_date',
 		'order'         => 'DESC'
 	);
-	$camps = get_posts( $args);
-	if (count($camps) == 0) {return '';}
-	else {
-		return get_the_title($camps[0]->ID);
-	}
+	return get_posts( $args);
+
 }
 function export_users() {
 	$array = array();
 	$query = get_users(array('fields'=>array('ID','display_name')));
 	foreach($query as $user) {
-		$array[] = array(find_post($user->ID),$user->ID);
+		$array[] = array(get_the_title(find_post($user->ID)),$user->ID);
 	}
 	download_send_headers("data_export_" . date("Y-m-d") . ".csv");
-	echo array2csv($array);
+	echo array2csv($array,array('User ID','Campaign Name'));
 	die();
 }
 
 add_action('give_campaign_export','export_users');
 //***************************************************************
+//Functions for exporting a csv of various participant and team stats
+
+function parts2csv(array &$array) {
+	if (count($array) == 0) {
+		return null;
+	}
+	ob_start();
+	echo "\xEF\xBB\xBF"; // UTF-8 BOM
+	$df = fopen("php://output", 'w');
+	foreach ($array as $row) {
+		fputcsv($df, $row);
+	}
+	fclose($df);
+	return ob_get_clean();
+}
+
+//calculate stats for indivdual participants not part of a team
+function format_ind_stats() {
+	$array = array();
+
+	//Section Title
+	$array[] = array('INDIVIDUALS');
+
+	$args = array(
+		'post_type' => 'give_forms',
+		'tax_query' => array(
+			'relation' => 'AND',
+			array(
+				'taxonomy' => 'give_forms_category',
+				'field' => 'slug',
+				'terms' => date('Y'),
+			),
+			array(
+				'taxonomy' => 'give_forms_category',
+				'field' => 'slug',
+				'terms' => 'active',
+			),
+		)
+	);
+	$camps = get_posts($args);
+
+	$total = 0; //Grand total
+	
+	foreach ($camps as $camp) {
+		$frm = new Give_Donate_Form($camp->ID);
+		$total += $frm->get_earnings(); // piggybacking to calculate grand total of all campaign earnings
+
+		if (!get_the_terms($camp,'give_forms_tag')) { // if no tags(ie not part of a team)
+			$array[] = array('',$camp->post_title . ' (ID: ' . $camp->post_author . ')', '$' . $frm->get_earnings());
+		}
+
+	}
+	$array[] = array();
+	$array[] = array('GRAND TOTAL','$'.$total);
+	return $array;
+}
+
+//calculate and return stats for teams and their members
+function format_team_stats($teams) {
+	$array = array();
+
+	//Section Title
+	$array[] = array('TEAMS');
+
+	//iterate through list of $teams
+	foreach ($teams as $team) {
+		$args = array(
+			'post_type' => 'give_forms',
+			'tax_query' => array(
+				'relation' => 'AND',
+				array(
+					'taxonomy' => 'give_forms_category',
+					'field' => 'slug',
+					'terms' => date('Y'),
+				),
+				array(
+					'taxonomy' => 'give_forms_category',
+					'field' => 'slug',
+					'terms' => 'active',
+				),
+				array(
+					'taxonomy' => 'give_forms_tag',
+					'field' => 'name',
+					'terms' => $team,
+				)
+			)
+		);
+		$members = get_posts($args);
+
+		if (count($members) !=0) { // don't output if this team has no members
+			//Team member count
+			$array[] = array($team, count($members) . ((count($members) == 1)? ' member':' members'));
+
+			$team_income = 0;
+
+			foreach ($members as $member) {
+				$frm = new Give_Donate_Form($member->ID);
+				$array[] = array('',$member->post_title . ' (ID: ' . $member->post_author . ')', '$' . $frm->get_earnings());
+				$team_income += $frm->get_earnings(); //aggregating earnings of all team members
+			}
+
+			$array[] = array();
+			$array[] = array('', 'Team Total', '$' . $team_income);
+			$array[] = array();
+		}
+	}
+	return $array;
+}
+
+//
+function export_participant_stats() {
+	$array = array();
+
+	$query = find_post('');
+	$num_participants = count($query)-1;
+	$array[] = array('# of Participants:',$num_participants);
+
+	$teams = get_terms( array(
+		'taxonomy' => 'give_forms_tag',
+		'fields'	=> 'names',
+		'hide_empty' => true,
+	));
+	$array[] = array('# of Teams:',count($teams));
+
+	//calculate stats for teams and their members
+	$team_stats = format_team_stats($teams);
+	foreach ($team_stats as $team_stat) {
+		$array[] = $team_stat;
+	}
+
+	//calculate stats for individuals not part of a team
+	$ind_stats = format_ind_stats();
+	foreach ($ind_stats as $ind_stat) {
+		$array[] = $ind_stat;
+	}
+
+	//export and exit
+	download_send_headers("data_export_" . date("Y-m-d") . ".csv");
+	echo parts2csv($array);
+	die();
+}
+add_action('give_participant_stats_export','export_participant_stats');
